a63cb9b378eb736454a0638354deb85b0e57bfa8
lakshyarajpurohit <130346936+lakshyarajpurohit@users.noreply.github.com>
2025-04-22 22:41:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import os
+from groq import Groq
+import random
+
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory    # this we set like k = 5 means it will store last 5 conversationso
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+groq_api_key = os.environ['GROQ_API_KEY']
+
+def main():
+
+    st.title("Groq Chat App")
+
+    # Add customization options to the sidebar
+    st.sidebar.title('Select an LLM')
+    model = st.sidebar.selectbox(
+        'Choose a model',
+        ['llama3-70b-8192', 'llama3-8b-8192']
+    )
+    conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+
+    memory=ConversationBufferWindowMemory(k=conversational_memory_length)
+
+    user_question = st.text_area("Ask a question:")
+
+        # session state variable
+    if 'chat_history' not in st.session_state:   # session state means how much conversationn is done 
+        st.session_state.chat_history=[]
+    else:
+        for message in st.session_state.chat_history:
+            memory.save_context({'input':message['human']},{'output':message['AI']})
+
+    # Initialize Groq Langchain chat object and conversation
+    groq_chat = ChatGroq(
+            groq_api_key=groq_api_key, 
+            model_name=model
+    )
+
+    conversation = ConversationChain(
+            llm=groq_chat,
+            memory=memory
+    )
+
+    if user_question:      # if question is being asked
+        response = conversation(user_question)
+        message = {'human':user_question,'AI':response['response']}
+        st.session_state.chat_history.append(message)
+        st.write("Chatbot:", response['response'])
+
+if __name__ == "__main__":
+    main()
